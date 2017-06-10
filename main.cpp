5a873f87465cb3bf405035f4ac07511e58f718ba
vmorales02 <vm200102@gmail.com>
2017-06-10 09:56:38 -0400

    mi primer commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+#include <string>
+using namespace std;
+int main() {
+  float alto, ancho, area; 
+  cout << "Ingresa un numero" << endl;
+  cin >> alto;
+  cout << "Ingresa numero" << endl;
+  cin >> ancho;
+  area = alto * ancho;
+  cout << "El resultado de area es: " << area << endl;
+}
