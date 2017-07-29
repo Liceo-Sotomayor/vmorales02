cdc8f4a2b9cdfea5474adee5b5f03b286b532d5b
vmorales02 <vm200102@gmail.com>
2017-07-29 11:16:42 -0400

    palabra

---
+++ palindromos.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string palabra;
+    cout << " ingrese una palabra " << endl;
+    cin >> palabra;
+    for (int i=0; i<palabra.size; i++) {
+    cout << palabra [0] << palabra.size() << endl;
+    }
+}
