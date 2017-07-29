fcb7cd593b2f8c40ea5da400c2a90c52d69b3a0a
vmorales02 <vm200102@gmail.com>
2017-07-29 10:07:43 -0400

    bucle

--- rol.cpp
+++ rol.cpp
@@ -1,24 +1,28 @@
-#include <iostream> 
-#include <cmath>
+#include <iostream>
+#include <string>
 #include <stdlib.h>
 #include <time.h>
 using namespace std;
 int main() {
-  srand (time(NULL));
-  bool esMayor, esIgual;
-  int a, b; 
-  a = rand()%4;
-  b = rand()%4;
-  esMayor = a > b;
-  esIgual = a == b;
-  
-  if (esIgual) {
-    cout << a << " Es igual a " << b << endl;
-  } else {
-    if (esMayor) {
-      cout << a << " Es mayor " << b << endl;
-    } else {
-      cout << a << " Es menor " << b << endl;
-    }
+    srand (time(NULL));
+    int arma, escudo, dano;
+    int vida=100; 
+    while (vida>0){
+      arma = rand()%6+1;
+      arma += rand()%6+1;
+      arma += 3;
+
+      escudo = rand()%4+1;
+      escudo += rand()%4+1;
+      escudo += 1;
+
+      dano = arma - escudo;
+      if (dano > 0) {
+          vida -= dano;
+          cout << " dano es " << dano << " vida restante " << vida << endl;
+      } else {
+          cout << " no sufrio dano "  << endl;
+      }
   }
 }
+  
